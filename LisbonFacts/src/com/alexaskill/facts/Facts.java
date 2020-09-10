c2c282e4e65eb17346eb75ad917bdd1ddd7f010f
Hugo <hugo.j.v.batista@outlook.com>
2020-09-10 21:18:03 +0100

    Fixed typos

--- LisbonFacts/src/com/alexaskill/facts/Facts.java
+++ LisbonFacts/src/com/alexaskill/facts/Facts.java
@@ -7,7 +7,7 @@ public class Facts {
                 "as a whole. Lisbon was founded by the Phoenicians in 1200 BC, a people of seafarers and merchants. " +
                 "The location on the Tagus river and on the Atlantic coast was apparently too good to miss out of. One " +
                 "of the theories about the emergence of the name Lisbon is that it comes from the Phoenician pronunciation " +
-                "Allis Ubbo, which means safe haven",
+                "Allis Ubbo, which means safe haven.",
         "Technically, Lisbon is not the capital of Portugal. There are no official documents that confirm that. " +
                 "Apparently it happened by chance when King Afonso the third settled with his court in Lisbon. That made " +
                 "sense of course, since Lisbon was already the largest and most important city in Portugal.",
@@ -17,7 +17,7 @@ public class Facts {
         "Many streets and squares in Lisbon are tiled in black and white. This is said to have its origins at Sao Vicente: " +
                 "white represents the clothing of the Crusaders, while black was the favorite colour of Sao Vicente.",
         "Like Rome, Lisbon is built on seven hills, and is called cidade das sete colinas, the city on seven hills." +
-                " Yet there are many who doubt this: are there seven hills? Or is it eight? When do you call something a hill exactly?",
+                " Yet there are many who doubt this: are there seven hills? Or is it eight?",
         "The Vasco da Gama bridge is the longest bridge in Europe with over 11 miles. Related fact: the Guinness Book Of " +
                 "Records writes about a dinner that 15000 people attended. Where was that dinner? Yep, on the Ponte Vasco" +
                 " da Gama, at the opening in 1998.",
@@ -26,7 +26,7 @@ public class Facts {
                 "finished in 1966. It's obvious that all kinds of things were going on during the intervening years: curses, " +
                 "dead architects and other priorities almost made this a prayer without end.",
         "The football club SL Benfica has the biggest fan club in the world. The number of official fans is estimated at more " +
-                "than 14 million. Of these 14 million there are 170 000 officially recognised as Socio.",
+                "than 14 million. Of these 14 million there are 170000 officially recognised as socio.",
         "Lisbon was the first city in the world to import Guinness beer from Dublin. There are other documents that tell us " +
                 "that Guinness was already shipped to Lisbon in 1811.",
         "Portugal in general and Lisbon in particular is crazy about football! That is why there are no less than 214 " +
@@ -38,32 +38,32 @@ public class Facts {
                 "was destroyed by this quake with a magnitude of 9 on the Richter scale. Estimates of victims in " +
                 "Lisbon range from 10000 to 100000.",
         "The oceanarium in Lisbon is one of the largest aquariums in the world. The large basin has more than 5" +
-                " million water. The tank is 20 feet deep and has a diameter of roughly a hundred feet. You should " +
+                " million litres of water. The tank is 20 feet deep and has a diameter of roughly a hundred feet. You should " +
                 "definitely see that yourself.",
         "Both the oldest bookstore in the world (Livraria Bertrand, 1732) and the smallest (Livraria do Simao, less" +
                 " than 35 square feet) can be found in Lisbon.",
         "During the Second World War, Portugal was neutral. This made the country extremely attractive for rich " +
                 "refugees. Especially the casino in Estoril, west of Lisbon, was a popular place. The fact that a " +
                 "lot of spies were sent there from all the intelligence services shouldn't come as a surprise. The " +
-                "most famous of them has to be Ian Fleming. Yes, that Ian Fleming.",
+                "most famous of them has to be Ian Fleming.",
         "Did you know that the casino of Estoril is the largest casino in Europe?",
         "On Rossio square you can find the only public clothing mirror in the world (as far as we can tell). For " +
                 "real. You can use this mirror to fashion your clothes, or your hair, or whatever. On the mirror " +
                 "it says componha o no da sua gravata, which translates to compose the knot of your tie.",
-        "Only five people in the world know the recipe of the Pastel de Belem, de originele Pastel de Nata. the " +
+        "Only five people in the world know the recipe of the Pastel de Belem, o original Pastel de Nata. the " +
                 "original Pastel de Nata. They have memorized the recipe, there is no written version. These five" +
                 " men therefore never travel simultaneously with the same aircraft or car. Just imagine.",
         "In Cafe Martinho Da Arcada, a classic Lisbon cafe, there are two tables where you can't sit down, they are " +
                 "permanently reserved for two of Portugal's most famous writers: one for Fernando Pessoa and one for" +
                 " Jose Saramago. Incidentally, you can just join Pessoa outside of Cafe A Brasileira.",
         "Not only Ponte 25 de Abril seems to have its roots in San Francisco (Golden Gate bridge), city officials " +
-                "were looking at San Fran for another symbol of Lisbon: the well-known yellow trams are modeled on " +
+                "were looking at San Francisco for another symbol of Lisbon: the well-known yellow trams are modeled on " +
                 "what they had in the mid-19th century in California. In 1873 the first tramway started in Lisbon," +
                 " and the trams were, appropriately, called Americanos.",
         "In ancient times it was quite normal to spot dolphins in the Tagus river. Nowadays it is a bit harder. This" +
-                " seems partly due to the pollution and I can imagine that all the bustle of shipping also doesn't " +
+                " seems partly due to the pollution and all the bustle of shipping also doesn't " +
                 "help. But, if you're really lucky you can still see them. You will have a better chance if you go " +
-                "down south south a bit further: from Setubal there are several companies that organise trips on the water.",
+                "down south a bit further: from Setubal there are several companies that organise trips on the water.",
         "Under the streets of Lisbon lies a genuine, 2000 year old Roman city, the Galerias Romanas. Really. With " +
                 "corridors, bridges and rooms. After the earthquake of 1755 this came to light during works there. " +
                 "And you won't believe your eyes when you see how you enter the place: through a small square metal" +
@@ -78,7 +78,7 @@ public class Facts {
                 "the Age of Discoveries as a military fortification, but many people don't know that it was used as a political" +
                 " prison during the Liberal Wars of the 19th century.",
         "Most people do know that locals from Lisbon are called Lisboetas, but many don't know the second nickname: Alfacinhas." +
-                " Alface is the Portuguese word for lettuce, and alfacinha means little lettuce'; the nickname is believed to " +
+                " Alface is the Portuguese word for lettuce, and alfacinha means little lettuce; the nickname is believed to " +
                 "stem from a time when residents had little else to eat than vegetables and preferred lettuce.",
         "Fado is a music genre originated in Portugal, derived from the Latin word fatum - destiny. The locals often arrange " +
                 "concerts in the restaurants where tourists can enjoy tasty dinner and listen to this sad but beautiful tunes.",
@@ -86,7 +86,7 @@ public class Facts {
                 "bank of the river. It was inspired by the Christ the Redeemer statue in Rio de Janeiro in Brazil. The statue " +
                 "commemorates Portugal's survival of the second world war.",
         "The Largest Human National Flag was raised in 2006 at Lisbon's national stadium by 18788 people.",
-        "The Tagus river is Iberia's the largest river and its estuary at Lisbon, up to 14 kilometers wide, is said to be large enough to " +
+        "The Tagus river is Iberia's largest river and its estuary at Lisbon, up to 14 kilometers wide, is said to be large enough to " +
                 "contain all the warships in the world."
 
     };
